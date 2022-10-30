bc34614a649cb9da617ab182511fe60a1c0e76f4
Faisal Ahmed <99274988+FaisalAhmedNour@users.noreply.github.com>
2022-10-30 22:06:12 +0600

    Add files via upload

---
+++ Codeforces/C++/A. Cowardly Rooks.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; ++i)
+        {
+            int a, b;
+            cin >> a >> b;
+        }
+        if (m < n)
+        {
+            cout << "YES\n";
+        }
+        else
+            cout << "NO\n";
+    }
+}
